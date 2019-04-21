da231300e3debd867df3e247b820db44f3ff80ef
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2019-04-21 17:30:55 +0100

    feature: update test naming

--- tests/TimeFrom.Tests.cs
+++ tests/TimeFrom.Tests.cs
@@ -7,94 +7,103 @@ namespace moment.net.Tests
     public class TimeFromTests
     {
         [Test]
-        public void SmallSecondsTest()
+        public void TimeFromAFewSecondsTest()
         {
             var aFewSecondsAgo = DateTime.UtcNow.AddSeconds(-20);
             aFewSecondsAgo.FromNow().ShouldBe("few seconds ago");
         }
 
         [Test]
-        public void LargeSecondsTest()
+        public void TimeFromSecondsMoreThanHalfButLessThanAMinuteTest()
         {
             var largeSecondsAgo = DateTime.UtcNow.AddSeconds(-50);
             largeSecondsAgo.FromNow().ShouldBe("one minute ago");
         }
 
         [Test]
-        public void SmallMinutesTest()
+        public void TimeFromExactlyAMinuteTest()
         {
-            var afewMinutesAgo = DateTime.Now.AddMinutes(-1);
-            afewMinutesAgo.FromNow().ShouldBe("one minute ago");
+            var aFewMinutesAgo = DateTime.Now.AddMinutes(-1);
+            aFewMinutesAgo.FromNow().ShouldBe("one minute ago");
         }
 
         [Test]
-        public void LargeMinutesTest()
+        public void TimeFromADefiniteNumberOfMinutesTest()
         {
             var minutesAgo = DateTime.Now.AddMinutes(-15);
             minutesAgo.FromNow().ShouldBe("15 minutes ago");
         }
 
         [Test]
-        public void HourTest()
+        public void TimeFromMinutesThatCanBeRoundedUpOrDownToAnHourTest()
         {
             var dateTime = DateTime.UtcNow.AddMinutes(-65);
             dateTime.FromNow().ShouldBe("one hour ago");
         }
 
         [Test]
-        public void MultipleHoursTest()
+        public void TimeFromADefiniteNumberOfHoursTest()
         {
             var dateTime = DateTime.UtcNow.AddHours(-20);
             dateTime.FromNow().ShouldBe("20 hours ago");
         }
 
         [Test]
-        public void DayTest()
+        public void TimeFromHoursThatCanBeRoundedUpOrDownToADayTest()
         {
             var dateTime = DateTime.UtcNow.AddHours(-25);
             dateTime.FromNow().ShouldBe("one day ago");
         }
 
         [Test]
-        public void MultipleDaysTest()
+        public void TimeFromADefiniteNumberOfDaysTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(-4);
             dateTime.FromNow().ShouldBe("4 days ago");
         }
 
         [Test]
-        public void MonthTest()
+        public void TimeFromDaysThatCanBeRoundedUpOrDownToAMonthTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(-27);
             dateTime.FromNow().ShouldBe("one month ago");
         }
 
         [Test]
-        public void MultipleMonthsTest()
+        public void TimeFromMultipleMonthsTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(-60);
             dateTime.FromNow().ShouldBe("2 months ago");
         }
 
         [Test]
-        public void YearTest()
+        public void TimeFromDaysAddingUpToAYearTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(-360);
             dateTime.FromNow().ShouldBe("one year ago");
         }
 
         [Test]
-        public void TwoYearsTest()
+        public void TimeFromMultipleYearsTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(-570);
             dateTime.FromNow().ShouldBe("2 years ago");
         }
 
         [Test]
-        public void MultipleYearsTest()
+        public void TimeFromMultipleYearsV2Test()
         {
             var dateTime = DateTime.UtcNow.AddDays(-3650);
             dateTime.FromNow().ShouldBe("10 years ago");
+        }
+
+        [Test]
+        public void TimeFromASpecifiedDateTimeTest()
+        {
+            var twoThousandAndTwelve = new DateTime(2012, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var twoThousandAndEighteen = new DateTime(2018, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            twoThousandAndTwelve.From(twoThousandAndEighteen).ShouldBe("6 years ago");
         }
     }
 }
--- tests/TimeTo.Tests.cs
+++ tests/TimeTo.Tests.cs
@@ -7,107 +7,98 @@ namespace moment.net.Tests
     public class TimeToTests
     {
         [Test]
-        public void AFewSecondsInTheFutureTest()
+        public void TimeToAFewSecondsTest()
         {
             var aFewSecondsAgo = DateTime.UtcNow.AddSeconds(20);
             aFewSecondsAgo.ToNow().ShouldBe("in few seconds");
         }
 
         [Test]
-        public void MoreSecondsInTheFutureTest()
+        public void TimeToSecondsMoreThanHalfButLessThanAMinuteTest()
         {
             var largeSecondsAgo = DateTime.UtcNow.AddSeconds(50);
             largeSecondsAgo.ToNow().ShouldBe("in one minute");
         }
 
         [Test]
-        public void AFewMinutesInTheFutureTest()
+        public void TimeToExactlyAMinuteTest()
         {
             var afewMinutesAgo = DateTime.Now.AddMinutes(1);
             afewMinutesAgo.ToNow().ShouldBe("in one minute");
         }
 
         [Test]
-        public void MoreMinutesInTheFutureTest()
+        public void TimeToADefiniteNumberOfMinutesTest()
         {
             var minutesAgo = DateTime.Now.AddMinutes(15);
             minutesAgo.ToNow().ShouldBe("in 15 minutes");
         }
 
         [Test]
-        public void ToHourTest()
+        public void TimeToMinutesThatCanBeRoundedUpOrDownToAnHourTest()
         {
             var dateTime = DateTime.UtcNow.AddMinutes(65);
             dateTime.ToNow().ShouldBe("in one hour");
         }
 
         [Test]
-        public void ToMultipleHoursTest()
+        public void TimeToADefiniteNumberOfHoursTest()
         {
             var dateTime = DateTime.UtcNow.AddHours(20);
             dateTime.ToNow().ShouldBe("in 20 hours");
         }
 
         [Test]
-        public void ToDayTest()
+        public void TimeToHoursThatCanBeRoundedUpOrDownToADayTest()
         {
             var dateTime = DateTime.UtcNow.AddHours(25);
             dateTime.ToNow().ShouldBe("in one day");
         }
 
         [Test]
-        public void ToMultipleDaysTest()
+        public void TimeToADefiniteNumberOfDaysTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(4);
             dateTime.ToNow().ShouldBe("in 4 days");
         }
 
         [Test]
-        public void ToMonthTest()
+        public void TimeToDaysThatCanBeRoundedUpOrDownToAMonthTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(27);
             dateTime.ToNow().ShouldBe("in one month");
         }
 
         [Test]
-        public void ToMultipleMonthsTest()
+        public void TimeToMultipleMonthsTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(60);
             dateTime.ToNow().ShouldBe("in 2 months");
         }
 
         [Test]
-        public void ToYearTest()
+        public void TimeToDaysAddingUpToAYearTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(360);
             dateTime.ToNow().ShouldBe("in one year");
         }
 
         [Test]
-        public void ToTwoYearsTest()
+        public void TimeToMultipleYearsTest()
         {
             var dateTime = DateTime.UtcNow.AddDays(570);
             dateTime.ToNow().ShouldBe("in 2 years");
         }
 
         [Test]
-        public void ToMultipleYearsTest()
+        public void TimeToMultipleYearsV2Test()
         {
             var dateTime = DateTime.UtcNow.AddDays(3650);
             dateTime.ToNow().ShouldBe("in 10 years");
         }
 
         [Test]
-        public void FromSpecifiedDateTest()
-        {
-            var twoThousandAndTwelve = new DateTime(2012, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            var twoThousandAndEighteen = new DateTime(2018, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-            twoThousandAndTwelve.From(twoThousandAndEighteen).ShouldBe("6 years ago");
-        }
-
-        [Test]
-        public void ToSpecifiedDateTest()
+        public void TimeToASpecifiedDateTimeTest()
         {
             var twoThousandAndTwelve = new DateTime(2012, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             var twoThousandAndEighteen = new DateTime(2018, 1, 1, 0, 0, 0, DateTimeKind.Utc);
