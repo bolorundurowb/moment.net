4a1829f5c60a99cc81d14ce56f8be8f0660b1b9e
Yemi Kudaisi <yemikudaisi@gmail.com>
2019-05-08 11:02:43 +0100

    Implemented EndOf

--- README.md
+++ README.md
@@ -34,12 +34,12 @@ var future = new DateTime(2021, 1, 1);
 var relativeTime = past.From(future); // in 2 years
 ```
 
-- StartOfX
+- StartOf
 ```csharp
 var time = DateTime.Parse("5/1/2008 8:30:52 AM", System.Globalization.CultureInfo.InvariantCulture);
 var startOfMinute = date.StartOf(DateTimeAnchor.Minute);	//01/05/2008 08:30:00"
 var startOfHour = date.StartOf(DateTimeAnchor.Hour);		//01/05/2008 08:00:00" 
-var startOfDay = date.StartOf(DateTimeAnchor.Day);			//01/05/2008 00:00:00"
+var startOfDay = date.StartOf(DateTimeAnchor.Day);		//01/05/2008 00:00:00"
 var startOfWeek = date.StartOf(DateTimeAnchor.Week);		//27/04/2008 00:00:00" (previous month)
 var startOfMonth = date.StartOf(DateTimeAnchor.Month);		//01/05/2008 00:00:00"
 var startOfYear = date.StartOf(DateTimeAnchor.Year);		//01/01/2008 00:00:00"
---
+++ tests/EndOf.Tests.cs
@@ -0,0 +1,54 @@
+using System;
+using moment.net.Enums;
+using NUnit.Framework;
+using Shouldly;
+
+namespace moment.net.Tests
+{
+    public class EndOfTests
+    {
+        string dateString = "5/1/2008 8:30:52 AM";
+
+        [Test]
+        public void EndOfMinuteTest()
+        {
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            date.EndOf(DateTimeAnchor.Minute).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("01/05/2008 08:30:59");
+        }
+
+        [Test]
+        public void EndOfHourTest()
+        {
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            date.EndOf(DateTimeAnchor.Hour).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("01/05/2008 08:59:59");
+        }
+
+        [Test]
+        public void EndOfDayTest()
+        {
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            date.EndOf(DateTimeAnchor.Day).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("01/05/2008 23:59:59");
+        }
+
+        [Test]
+        public void EndOfWeekTest()
+        {
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            date.EndOf(DateTimeAnchor.Week).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("03/05/2008 23:59:59");
+        }
+
+        [Test]
+        public void EndOfMonthTest()
+        {
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            date.EndOf(DateTimeAnchor.Month).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("31/05/2008 23:59:59");
+        }
+
+        [Test]
+        public void EndOfYearTest()
+        {
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            date.EndOf(DateTimeAnchor.Year).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("31/12/2008 23:59:59");
+        }
+    }
+}
