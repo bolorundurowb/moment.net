2154d59760f0c404733a324d8d15aee2ff8225c4
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-04-24 10:23:43 +0200

    Add test for Kind on Next

--- tests/Next.Tests.cs
+++ tests/Next.Tests.cs
@@ -7,20 +7,22 @@ namespace moment.net.Tests
 {
     public class Next
     {
-        string dateString = "5/1/2008 8:30:52 AM";
+        string dateString = "5/1/2008 8:30:52Z AM";
 
         [Test]
         public void NextDayOfWeekTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
             date.Next(DayOfWeek.Thursday).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("08/05/2008 08:30:52");
+            date.Next(DayOfWeek.Thursday).Kind.ShouldBe(DateTimeKind.Utc);
         }
 
         [Test]
         public void NextNthDayOfWeekTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
-            date.Next(DayOfWeek.Thursday,3).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("22/05/2008 08:30:52");
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
+            date.Next(DayOfWeek.Thursday, 3).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("22/05/2008 08:30:52");
+            date.Next(DayOfWeek.Thursday, 3).Kind.ShouldBe(DateTimeKind.Utc);
         }
     }
 }
