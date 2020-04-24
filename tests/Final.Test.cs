d3da948e2c2586d25aa612adfbe6057b6a5b8d22
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-04-24 10:16:21 +0200

    Add test for Kind on Final

--- tests/Final.Test.cs
+++ tests/Final.Test.cs
@@ -7,20 +7,22 @@ namespace moment.net.Tests
 {
     public class Final
     {
-        string dateString = "5/1/2008 8:30:52 AM";
+        string dateString = "5/1/2008 8:30:52Z AM";
 
         [Test]
         public void FinalInMonthTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
             date.Final().Monday().InMonth().ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("26/05/2008 00:00:00");
+            date.Final().Monday().InMonth().Kind.ShouldBe(DateTimeKind.Utc);
         }
 
         [Test]
         public void FinalInYearTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
             date.Final().Sunday().InYear().ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("28/12/2008 00:00:00");
+            date.Final().Sunday().InYear().Kind.ShouldBe(DateTimeKind.Utc);
         }
     }
 }
