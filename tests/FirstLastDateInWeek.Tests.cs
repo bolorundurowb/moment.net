3827158fbfcfeeb5000f3afb47b4a1688c6734b0
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-04-24 10:20:07 +0200

    Add test for Kind on FirstLastDayInWeek

--- tests/FirstLastDateInWeek.Tests.cs
+++ tests/FirstLastDateInWeek.Tests.cs
@@ -7,20 +7,22 @@ namespace moment.net.Tests
 {
     public class FirstLastDateInWeek
     {
-        string dateString = "5/1/2008 8:30:52 AM";
+        string dateString = "5/1/2008 8:30:52Z AM";
 
         [Test]
         public void FirstDateInWeekTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
             date.FirstDateInWeek().ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("27/04/2008 00:00:00");
+            date.FirstDateInWeek().Kind.ShouldBe(DateTimeKind.Utc);
         }
 
         [Test]
         public void LastDateInWeekTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
             date.LastDateInWeek().ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("03/05/2008 00:00:00");
+            date.LastDateInWeek().Kind.ShouldBe(DateTimeKind.Utc);
         }
 
     }
