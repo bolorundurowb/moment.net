5f9700dcd4a84a2de863e0321ed346f7857ba281
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-04-24 10:20:46 +0200

    Remove stray whitespace

--- tests/Last.Tests.cs
+++ tests/Last.Tests.cs
@@ -12,14 +12,14 @@ namespace moment.net.Tests
         public void LastDayOfWeekTest()
         {
             DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
-            date.Last(DayOfWeek.Thursday).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("24/04/2008 08:30:52"); 
+            date.Last(DayOfWeek.Thursday).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("24/04/2008 08:30:52");
         }
 
         [Test]
         public void LastNthDayOfWeekTest()
         {
             DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
-            date.Last(DayOfWeek.Thursday,3).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("10/04/2008 08:30:52"); 
+            date.Last(DayOfWeek.Thursday,3).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("10/04/2008 08:30:52");
         }
     }
 }
