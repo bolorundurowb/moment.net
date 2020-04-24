8904fc9ba99617ef7e5cb67262901092961fc8ad
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-04-24 10:22:28 +0200

    Add test for Kind on Last

--- tests/Last.Tests.cs
+++ tests/Last.Tests.cs
@@ -6,20 +6,22 @@ namespace moment.net.Tests
 {
     public class Last
     {
-        string dateString = "5/1/2008 8:30:52 AM";
+        string dateString = "5/1/2008 8:30:52Z AM";
 
         [Test]
         public void LastDayOfWeekTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
             date.Last(DayOfWeek.Thursday).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("24/04/2008 08:30:52");
+            date.Last(DayOfWeek.Thursday).Kind.ShouldBe(DateTimeKind.Utc);
         }
 
         [Test]
         public void LastNthDayOfWeekTest()
         {
-            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
-            date.Last(DayOfWeek.Thursday,3).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("10/04/2008 08:30:52");
+            DateTime date = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.AdjustToUniversal);
+            date.Last(DayOfWeek.Thursday, 3).ToString("dd/MM/yyyy HH:mm:ss").ShouldBe("10/04/2008 08:30:52");
+            date.Last(DayOfWeek.Thursday, 3).Kind.ShouldBe(DateTimeKind.Utc);
         }
     }
 }
