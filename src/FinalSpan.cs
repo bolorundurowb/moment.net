ced7b627a0fb169ebeaf2eba8774809eab81f547
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-04 15:48:36 +0100

    ch: whitespace fixes and rmeoval of unused classes

--- src/FinalSpan.cs
+++ src/FinalSpan.cs
@@ -7,34 +7,36 @@ namespace moment.net
     public class FinalSpan
     {
         private DateTime _dt;
-        private DayOfWeek _dow;
+        private readonly DayOfWeek _dayOfWeek;
 
         public FinalSpan(DateTime dateTime, DayOfWeek dayOfWeek)
         {
             _dt = dateTime;
-            _dow = dayOfWeek;
+            _dayOfWeek = dayOfWeek;
         }
 
         public DateTime InMonth()
         {
             var m = _dt.Month;
             // only loop through the last seven days of the month
-            _dt = new DateTime(_dt.Year,_dt.Month, (DateTime.DaysInMonth(_dt.Year, _dt.Month)-7), 0, 0, 0, _dt.Kind);
+            _dt = new DateTime(_dt.Year, _dt.Month, (DateTime.DaysInMonth(_dt.Year, _dt.Month) - 7), 0, 0, 0, _dt.Kind);
             while (_dt.Month == m)
             {
-                if(_dt.DayOfWeek == _dow)
+                if (_dt.DayOfWeek == _dayOfWeek)
                 {
                     return _dt;
                 }
+
                 _dt = _dt.AddDays(1);
             }
+
             return DateTime.MaxValue;
         }
 
         public DateTime InYear()
         {
             var dt = new DateTime(_dt.Year, 12, (DateTime.DaysInMonth(_dt.Year, 12) - 7), 0, 0, 0, _dt.Kind);
-            return new FinalSpan(dt, _dow).InMonth();
+            return new FinalSpan(dt, _dayOfWeek).InMonth();
         }
     }
 }
