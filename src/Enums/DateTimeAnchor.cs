ca717bfb83c2c4d389e161e9c7a78b2e0ec991a7
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2026-02-22 00:18:52 +0100

    ft: add summary documentation to DateTimeAnchor enum

--- src/Enums/DateTimeAnchor.cs
+++ src/Enums/DateTimeAnchor.cs
@@ -1,5 +1,9 @@
 ﻿namespace moment.net.Enums;
 
+/// <summary>
+/// Specifies anchors for date and time operations to define the starting point for calculations
+/// such as trimming or aligning a <see cref="DateTime"/> value.
+/// </summary>
 public enum DateTimeAnchor
 {
     Minute,
