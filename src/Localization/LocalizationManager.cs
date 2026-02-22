fd054f4a81aa57b61e7a6b56c6fba71ba51dad03
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2026-02-22 22:27:14 +0100

    ch: restructure localization files, refactor CultureWrapper and LocalizationManager classes

---
+++ src/Localization/LocalizationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace moment.net;
+
+internal class LocalizationManager : IDisposable
+{
+    private const string LocalisationResourceBaseName = "moment.net.Strings";
+    
+    private readonly CultureWrapper _cw;
+    private readonly ResourceManager _rm;
+
+    public LocalizationManager(CultureInfo ci)
+    {
+        _cw = new CultureWrapper(ci);
+        _rm = new ResourceManager(LocalisationResourceBaseName, Assembly.GetExecutingAssembly());
+    }
+
+    public string? GetString(string key) => _rm.GetString(key);
+
+    public void Dispose() => _cw.Dispose();
+}
